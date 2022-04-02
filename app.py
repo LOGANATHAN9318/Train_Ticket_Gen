ea9e23cf70dad01b1203ce4d4d8ca2455aa3b9de
LOGANATHAN9318 <loganathan9318@gmail.com>
2022-04-02 21:52:19 +0530

    New changes

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
+hide_menu = """
+<style>
+#MainMenu {
+    visibility:hidden;
+}
+footer{
+    visibility:hidden;
+}
+
+footer:after{
+    content:'Copyright @ 2022';
+    visibility:visible;
+    display:block;
+    position:relative;
+    color:tomato;
+}
+</style>
+"""
 
 
 def booking_ticket():
     st.subheader("Book Your Train Ticket")
-    From = st.text_input("From")  
-    to = st.text_input("To")    
+    station_list =['','Salem','Chennai','Villupuram','Madurai']
+    From = st.selectbox('From',station_list)  
+    to = st.selectbox('To',station_list)   
     board_sta = st.text_input("Boarding Station")        
     name = st.text_input("Passenger Name")
-
 
     
 def about_app():
@@ -16,16 +36,24 @@ def about_app():
 
     
 def main():
+    choice = option_menu(
+        menu_title=None,
+        options=["Home","About","Contact"],
+        icons=["house","book","envelope"],
+        menu_icon="cast",
+        default_index=0,
+        orientation="horizontal")
+
     st.title("Train Ticket Generator")
-    menu = ["Home","About"]
-    choice = st.sidebar.selectbox("Menu",menu)
+    st.markdown(hide_menu,unsafe_allow_html=True)
     
     if choice == 'Home':
         booking_ticket()
 
     elif choice == 'About':
         about_app()
-        
+
+
         
 if __name__ == '__main__':
     main()
